8e5ed983ca00d3d955b1eb6e69f10e975b869255
doris hutter <doris.hutter@gmx.de>
2021-07-05 15:58:56 +0200

    models

---
+++ models/Story.model.js
@@ -0,0 +1,27 @@
+// TODO: Please make sure you edit the story model to whatever makes sense in this case
+const storySchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+        trim: true,
+      },
+      body: {
+        type: String,
+        required: true,
+      },
+      duration: {
+        type: Number,
+      },
+      author: {
+        type: String,
+    },
+
+});
+
+const Story = model("Story", storySchema);
+
+module.exports = Story;
+
+
+
+//export default Story;
