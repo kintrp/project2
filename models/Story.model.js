d6dc8cc44e4b978a24694eef657e87391ffd5832
kintrp <philipp.kintrup@googlemail.com>
2021-07-05 17:12:05 +0200

    modelos

---
+++ models/Story.model.js
@@ -0,0 +1,23 @@
+
+/* const storySchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+        trim: true,
+      },
+      body: {
+        type: String,
+        required: true,
+      },
+      duration: {
+        type: Number,
+      },
+      author: {
+        type: String,
+    },
+
+});
+
+const Story = model("Story", storySchema);
+
+module.exports = Story; */
