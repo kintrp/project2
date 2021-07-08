841c20059b6b3087832af2b7fee2951b8fa5287b
doris hutter <doris.hutter@gmx.de>
2021-07-08 09:46:00 +0200

    styling profile

---
+++ public/js/mapbox.js
@@ -0,0 +1,37 @@
+mapboxgl.accessToken='pk.eyJ1Ijoia2ludHJwIiwiYSI6ImNrcXM1OWQ1ZjFta2Qybm1ocTk2cG84djMifQ.m7fogXVWDffNLgmhb0Pe5g'
+
+console.log('HelloFromMapBox');
+
+const map = new mapboxgl.Map({
+    container: 'map', // container ID
+    style:'mapbox://styles/kintrp/ckqs6ybuj0vh117odwy1wl36q',
+    //style: 'mapbox://styles/mapbox/streets-v11', // style URL
+    center: [13.405, 52.52], // starting position [lng, lat]
+    zoom: 10 // starting zoom,
+});
+
+// NavBAr
+
+const nav = new mapboxgl.NavigationControl();
+map.addControl(nav, 'top-left');
+
+// Axios
+
+axios.get(`http://localhost:3000/locations`)
+		.then(response => {
+			console.log(response.data);
+
+       
+
+
+		})
+		.catch(err => {
+			console.log(err);
+		})
+
+const marker = new mapboxgl.Marker({
+    color: 'red'
+})
+.setLngLat([13.405, 52.52])
+.addTo(map); 
+  
--- routes/stories.js
+++ routes/stories.js
@@ -3,12 +3,15 @@ const router = express.Router();
 const User = require('../models/User.model');
 const Story = require('../models/Story.model');
 
-// render profile
-// add story 
+// render profile hbs in profile url
 
+/* 
 router.get('/profile', (req, res, next) => {
-	res.render('profile', {user:req.session.user});
-});
+res.render('profile', {user:req.session.user});
+}); 
+*/
+
+// Create stories by profile
 
 router.post('/profile', (req, res, next) => {
     const {title, story, genre, city} = req.body;
@@ -22,6 +25,8 @@ router.post('/profile', (req, res, next) => {
   });
   });
 
+// Show stories in stories url
+
 router.get('/stories', (req, res, next) => {
 	Story.find()
 		.then(storyFromDB => {
@@ -33,5 +38,80 @@ router.get('/stories', (req, res, next) => {
 		})
 });
 
+// locations stories
+
+router.get('/locations', (req, res, next) => {
+  Story.find()
+		.then(storyFromDB => {
+      postalCodes = storyFromDB.map(e=>e.city)
+        console.log(postalCodes);
+			res.json({stories:postalCodes});
+		})
+		.catch(err => {
+			console.log(err)
+		})
+})
+
+// show stories in profile
+
+router.get('/profile', (req, res, next) => {
+    console.log('Hello');
+Story.find({author: req.session.user.username})
+  .then(storiesFromDB => {
+    console.log(storiesFromDB, req.session.user.username);
+  res.render('profile', { storyList: storiesFromDB, user:req.session.user });
+  })
+  })
+
+// details
+
+router.get('/stories/:id', (req,res, next)=> {
+  const storyId = req.params.id;
+  console.log(storyId);
+  Story.findById(storyId)
+    .then(storyDetails => {
+      res.render('details', {details : storyDetails })
+      console.log(storyDetails);
+    })
+    .catch(error => console.log('error while retrieving data from DB', error))
+}) 
+
+// delete the stories
+
+router.post('/stories/:id/delete', (req, res, next) => {
+	const storyId = req.params.id;
+	// delete this story	
+	Story.findByIdAndDelete(storyId)
+		.then(() => {
+			// redirect to the stories list
+			res.redirect('/stories');
+		})
+		.catch(err => {
+			console.log(err);
+		})
+}); 
+
+//editing stories
+
+/* router.get('/stories:id/edit', (req, res, next) => {
+  Story.findOneAndUpdate(req.params.id)
+    .then(stories => {
+      res.render('/profile', {stories});
+    })
+    .catch(err => {
+      next(err);
+    });
+}); */
+
+router.post('/:id/edit', (req, res) => {
+  const {id} = req.params;
+  const {title, story, genre, city} = req.body;
+
+  Story.findByIdAndUpdate(id, {title, story, genre, city}, {new: true})
+  .then(updatedStory =>res.redirect('/edit'))
+  .catch(error => next(error));
+});
+
 
 module.exports = router;
+
